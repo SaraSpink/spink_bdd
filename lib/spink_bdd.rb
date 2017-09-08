c92531edf01448358cca214480835233d0e2bbbd
Sara Spink <spinkbot@gmail.com>
2017-09-08 12:07:53 -0700

    stop being dyslexic and work on anagrams instead of palindromes

--- lib/spink_bdd.rb
+++ lib/spink_bdd.rb
@@ -1,29 +1,52 @@
 #! usr/bin/env ruby
 class Anagram
-  def initialize ()
+  def initialize
   end
 
   def anagram_maker(word)
-    word.reverse()
+    newarray = word.chars
+    newarray.sort
   end
 
-  def anagram_array(word1, word2)
+  def anagram_evaluator(input1, input2)
     compare = []
-    compare.push(word1.downcase)
-    compare.push(word2.downcase)
+    compare.push(input1.downcase)
+    compare.push(input2.downcase)
 
     compare1 = compare.at(0)
     compare2 = compare.at(1)
 
     if compare1 === compare2
-      puts "#{word2} is not an anagram of #{word1}"
       return false
     elsif compare1 === anagram_maker(compare2)
-      puts "#{word2} is an anagram of #{word1}!"
       return true
     end
+
   end
-
-
-
 end
+
+
+################ ORIGINAL CODE ##################
+# class Anagram
+#   def initialize
+#   end
+#
+#   def anagram_maker(word)
+#     word.reverse()
+#   end
+#
+#   def anagram_array(word1, word2)
+#     compare = []
+#     compare.push(word1.downcase)
+#     compare.push(word2.downcase)
+#
+#     compare1 = compare.at(0)
+#     compare2 = compare.at(1)
+#
+#     if compare1 === compare2
+#       return false
+#     elsif compare1 === anagram_maker(compare2)
+#       return true
+#     end
+#   end
+# end
--- spec/spink_bdd_spec.rb
+++ spec/spink_bdd_spec.rb
@@ -5,19 +5,31 @@ require ('spink_bdd')
 newanagram = Anagram.new()
 
 describe('#spink_bdd') do
-  it ("takes a word and returns the word in reverse") do
-    expect(newanagram.anagram_maker("cat")).to(eq("tac"))
+  it ("returns false if two words are exactly the same") do
+    expect(newanagram.anagram_evaluator("bear", "bear")).to(eq(false))
   end
 
-  it ("returns false if two words are not anagrams") do
-    expect(newanagram.anagram_array("dog", "dog")).to(eq(false))
+  it ("takes a word and returns a sorted array") do
+    expect(newanagram.anagram_maker("bear")).to(eq(["a", "b", "e", "r"]))
   end
-
-  it ("returns true if two words are the reverse of one another") do
-    expect(newanagram.anagram_array("dog", "god")).to(eq(true))
-  end
-
-  it ("accounts for case sensitivity") do
-    expect(newanagram.anagram_array("Dog", "god")).to(eq(true))
-  end
+  ############### ORIGINAL TESTS #################
+  # it ("takes a word and returns the word in reverse") do
+  #   expect(newanagram.anagram_maker("cat")).to(eq("tac"))
+  # end
+  #
+  # it ("returns false if two words are not anagrams") do
+  #   expect(newanagram.anagram_array("dog", "dog")).to(eq(false))
+  # end
+  #
+  # it ("returns true if two words are the reverse of one another") do
+  #   expect(newanagram.anagram_array("dog", "god")).to(eq(true))
+  # end
+  #
+  # it ("accounts for case sensitivity") do
+  #   expect(newanagram.anagram_array("Dog", "god")).to(eq(true))
+  # end
+  #
+  # it ("returns true if a phrase is a palindrome regardless of spaces or punctuation") do
+  #   expect(newanagram.anagram_array("a dOg", "God a")).to(eq(true))
+  # end
 end
