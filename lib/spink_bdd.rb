eab99da16a2a37b6a6546612facadab67d883905
Sara Spink <spinkbot@gmail.com>
2017-09-08 10:11:37 -0700

    pass a test that returns false if two arguments are the same and therefore not anagrams

--- lib/spink_bdd.rb
+++ lib/spink_bdd.rb
@@ -1,6 +1,27 @@
 #! usr/bin/env ruby
 class Anagram
+  def initialize ()
+  end
+
   def anagram_maker(word)
     word.reverse()
   end
+
+  def anagram_array(word1, word2)
+    compare = []
+    compare.push(word1)
+    compare.push(word2)
+
+    compare1 = compare.at(0)
+    compare2 = compare.at(1)
+
+    if compare1 === compare2
+      return false
+    else
+      return true
+    end
+  end
+
+
+
 end
--- spec/spink_bdd_spec.rb
+++ spec/spink_bdd_spec.rb
@@ -8,4 +8,8 @@ describe('#spink_bdd') do
   it ("takes a word and returns the word in reverse") do
     expect(newanagram.anagram_maker("cat")).to(eq("tac"))
   end
+
+  it ("returns false if two words are not anagrams") do
+    expect(newanagram.anagram_array("dog", "dog")).to(eq(false))
+  end
 end
