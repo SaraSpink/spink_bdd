04eb393149c09e8993395bf9f07255d9252dc723
Guest User <Guest@Epicodus-3A.local>
2017-09-12 18:22:23 -0700

    clean up and pass all specs and create a working method for antigrams

--- lib/spink_bdd.rb
+++ lib/spink_bdd.rb
@@ -57,56 +57,13 @@ class Anagram
   def antigram_evaluator
     anti1 = @firstword.downcase.gsub(/\s+/, "")
     anti2 = @secondword.downcase.gsub(/\s+/, "")
-    compare1 = anti1.chars.sort
-    compare2 = anti2.chars.sort
-    if anti1 == anti2
-      return false
+
+    if (anti1.chars & anti2.chars).empty?
+      puts "this is an antigram"
+      true
     else
-      return true
+      false
     end
 
   end
-  #   compare = []
-  #   compare.push(@firstword.downcase)
-  #   compare.push(@secondword.downcase)
-  #
-  #   compare1 = compare.at(0)
-  #   compare2 = compare.at(1)
-  #
-  #
-  #
-  #   if compare1 === compare2
-  #     return false
-  #   else anagram_maker(compare1) != anagram_maker(compare2)
-  #     puts "This is an antigram"
-  #     return true
-  #   end
-  # end
-
 end
-
-
-################ ORIGINAL CODE ##################
-# class Anagram
-#   def initialize
-#   end
-#
-#   def anagram_maker(word)
-#     word.reverse()
-#   end
-#
-#   def anagram_array(word1, word2)
-#     compare = []
-#     compare.push(word1.downcase)
-#     compare.push(word2.downcase)
-#
-#     compare1 = compare.at(0)
-#     compare2 = compare.at(1)
-#
-#     if compare1 === compare2
-#       return false
-#     elsif compare1 === anagram_maker(compare2)
-#       return true
-#     end
-#   end
-# end
--- spec/spink_bdd_spec.rb
+++ spec/spink_bdd_spec.rb
@@ -4,48 +4,55 @@ require ('spink_bdd')
 
 
 
-describe('#spink_bdd') do
-  a = Anagram.new("bear", "bear")
-  it ("returns false if two words are exactly the same") do
-    expect(a.anagram_evaluator()).to(eq(false))
-  end
-
+describe ('Anagram#anagram_maker') do
   it ("takes a word and returns a sorted array") do
-    expect(a.anagram_maker("bear")).to(eq(["a", "b", "e", "r"]))
+    newanagram = Anagram.new("bear", "bear")
+    expect(newanagram.anagram_maker("bear")).to(eq(["a", "b", "e", "r"]))
+  end
+end
+
+describe('Anagram#anagram_evaluator') do
+  it ("returns false if two words are exactly the same") do
+    newanagram = Anagram.new("bear", "bear")
+    expect(newanagram.anagram_evaluator()).to(eq(false))
   end
 
-  b = Anagram.new("a ruby", "a bury")
   it ("returns true if two words or phrases contain the same letters and spaces")do
-    expect(b.anagram_evaluator()).to(eq(true))
+    newanagram = Anagram.new("a ruby", "a bury")
+    expect(newanagram.anagram_evaluator()).to(eq(true))
   end
 
-  c = Anagram.new("a dog", "A God")
   it ("accounts for case sensitivity") do
-    expect(c.anagram_evaluator()).to(eq(true))
+    newanagram = Anagram.new("a dog", "A God")
+    expect(newanagram.anagram_evaluator()).to(eq(true))
   end
 
-  it ("takes a word and returns the word in reverse") do
-    expect(a.palindrome_maker("cat")).to(eq("tac"))
-  end
-
-  d = Anagram.new("live", "evil")
   it ("returns true if two words are the reverse of one another") do
-    expect(d.palindrome_evaluator()).to(eq(true))
+    newanagram = Anagram.new("live", "evil")
+    expect(newanagram.palindrome_evaluator()).to(eq(true))
+  end
+end
+
+describe('Anagram#palindrome_evaluator') do
+  it ("takes a word and returns the word in reverse") do
+    newanagram = Anagram.new("cat", "tac")
+    expect(newanagram.palindrome_maker("cat")).to(eq("tac"))
   end
 
-  it ("determines if a word is truly a word and returns false if a word has no vowels") do
-    expect(d.isword("zxzy")).to(eq(false))
+  it ("takes a word or group of words and returns the reverse") do
+    newanagram = Anagram.new("live", "e vil")
+    expect(newanagram.palindrome_evaluator()).to(eq(true))
   end
+end
 
-  e = Anagram.new("Baby", "duck")
+describe('Anagram#antigram_evaluator') do
   it ("is an antigram if no letters match") do
-    expect(d.antigram_evaluator).to(eq(true))
+    newanagram = Anagram.new("Baby", "duck")
+    expect(newanagram.antigram_evaluator).to(eq(true))
   end
 
-  f = Anagram.new("fabled", "ooh boy")
   it ("accounts for multiple words being antigrams and returns true if the words are antigrams") do
-    expect(f.antigram_evaluator).to(eq(true))
+    newanagram = Anagram.new("fabled", "too much") 
+    expect(newanagram.antigram_evaluator).to(eq(true))
   end
-
-
 end
